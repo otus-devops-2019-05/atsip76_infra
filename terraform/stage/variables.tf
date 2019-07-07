9b730e16b3bd25467ff09a20ee057d4c5dc2de2a
Anatoly Afanasyev <atsip76@gmail.com>
2019-07-07 20:36:50 +0500

    HW7 part 1

---
+++ terraform/stage/variables.tf
@@ -0,0 +1,40 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-west1"
+}
+
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
+
+variable zone {
+  description = "Zone"
+  default     = "europe-west1-b"
+}
+
+variable private_key_path {
+  description = "Path to the private_key used for ssh access"
+}
+
+variable count_instance {
+  description = "count of instances"
+  default     = "1"
+}
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
+}
