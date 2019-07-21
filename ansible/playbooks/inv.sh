e7a65df9c688c502e24c96ad1dfb79fb8eb90e1f
Anatoly Afanasyev <atsip76@gmail.com>
2019-07-21 23:29:11 +0500

    HW10

--- ansible/playbooks/inv.sh
+++ ansible/playbooks/inv.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 dynamic_json_file=$(gcloud compute instances list | tail -n +2 | tr -s ' ' | cut -d ' ' -f1,5)
 internal_ip=$(gcloud compute instances list | tail -n +2 | tr -s ' ' | cut -d ' ' -f1,4)
+db_internal_ip=$(echo ${internal_ip} | awk -F"db " '{ print $2 }')
+/bin/sed -i "s/db_host: *.*.*.*/db_host: ${db_internal_ip}/" ~/OTUS/DEVOPS/atsip76_infra/ansible/\
+roles/app/defaults/main.yml
+/bin/sed -i "s/mongo_bind_ip: *.*.*.*/mongo_bind_ip: 0.0.0.0/" ~/OTUS/DEVOPS/atsip76_infra/ansible/\
+roles/db/defaults/main.yml
+
 
 if [ "$1" == "--list" ] ; then
         app=$(echo ${dynamic_json_file} | awk -F"app | reddit" '{ print $2 }')
